3e58801bf0f9aafd9cffbc77d9b55a0f2374b858
RezaFarzipour <rezafafa78@gmail.com>
2025-03-22 20:13:40 +0330

    getCookie function added to cookie file

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -2,5 +2,15 @@ const setCookie = (cookieName:string,token:string)=>{
     document.cookie = `${cookieName} =${token}`
 }
 
+function getCookie(name: string): string | null {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+    return null;
+}
 
-export {setCookie}
+export {setCookie,getCookie}
