aeead8c3e439b78e5be543e7a44e4f7e27fe18e0
RezaFarzipour <rezafafa78@gmail.com>
2025-04-10 13:38:32 +0330

    react query config updated farsi to english function added to utils

--- src/configs/reactQuery.ts
+++ src/configs/reactQuery.ts
@@ -1,8 +1,9 @@
 const defaultOptions = {
     queries: {
       refetchOnWindowFocus: false,
-      retry: 1,
-      staleTime: 60  * 3* 1000,
+    retry: 1,
+    staleTime: 0, // چون می‌خوای همیشه stale باشه که هربار refetch بشه
+    refetchInterval: 30 * 1000, // هر 30 ثانیه یه بار refetch
     }
   };
 
--- src/utils/numberFormatter.ts
+++ src/utils/numberFormatter.ts
@@ -4,10 +4,18 @@ export function toPersianDigits(n: number | string): string {
 }
 
 
-export const priceSeptrator = (num: number ): string => {
+export const priceSeptrator = (num: number |string ): string => {
   return num.toLocaleString("en-US");
 };
 
 export const deleteCookie = (name:string) => {
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
+
+
+export const FarsiToEnglishNumber = (str: string): string => {
+  const farsiDigits = "۰۱۲۳۴۵۶۷۸۹";
+  return str
+    .replace(/[^\d۰-۹]/g, "") // فقط عدد (فارسی یا انگلیسی) رو نگه می‌داریم
+    .replace(/[۰-۹]/g, (d) => farsiDigits.indexOf(d).toString());
 };
