2ab604ec7537245ce5d8578331e071c1720f3b25
RezaFarzipour <rezafafa78@gmail.com>
2025-04-15 16:50:09 +0330

    verifycode and verifynumber updated

--- src/components/template/verifycode/VerifyCodeTemp.tsx
+++ src/components/template/verifycode/VerifyCodeTemp.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Paper, Box, Typography, Grid2 } from "@mui/material";
 import VerifyCodeImage from "../../../assets/images/pexels-michael-steinberg-321464.jpg";
-import {
-  EditNumberBtn,
-  InnerGrid,
-  MainVerifyBox,
-  VerifyMainGrid,
-} from "./style";
+import { InnerGrid, MainVerifyBox, VerifyMainGrid } from "./style";
 import { Link, useNavigate } from "react-router-dom";
 import VerifyInput from "../../element/auth/verifyNumberCode-input";
 import VerifyButton from "../../element/auth/verifyNumberCode-button";
-
+import EditIcon from "@mui/icons-material/Edit";
 import { checkOtp } from "../../../services/auth";
 import { getCookie, setCookie } from "../../../utils/cookie";
 import { Rtl } from "../../element/rtl";
+import Alerts from "../../element/AlertElement";
 
 const VerifyCodePage = () => {
   const [verifyCode, setVerifyCode] = React.useState<string>("");
   const [loading, setLoading] = React.useState(false);
+  const [alertHandler, setAlertHandler] = useState({
+    severity: "",
+    text: "",
+  });
 
   const navigate = useNavigate();
   const token = getCookie("phone-number");
@@ -27,9 +27,14 @@ const VerifyCodePage = () => {
     setLoading(true);
 
     const { response, err } = await checkOtp(token, verifyCode);
-    if (response) {
+
+    if (response && response.status === 200) {
       setCookie("token", response.data.token);
       setCookie("user_type", response.data.user_type);
+      setAlertHandler({
+        text: "ورود موفقیت آمیز ",
+        severity: "success",
+      });
 
       if (response.data.signup_require) {
         navigate("/signupinfo");
@@ -46,13 +51,19 @@ const VerifyCodePage = () => {
       }
     } else if (err) {
       setLoading(false);
-      console.log(err);
+      setAlertHandler({
+        text: "خطایی رخ داده است یا کد ورود  اشتباه است",
+        severity: "error",
+      });
     }
   };
 
   return (
     <Rtl>
       <Grid2 container sx={VerifyMainGrid(VerifyCodeImage)}>
+        {!!alertHandler.text && (
+          <Alerts text={alertHandler.text} severity={alertHandler.severity} />
+        )}
         <Grid2
           sx={InnerGrid}
           size={{ xs: 12, sm: 8, md: 5 }}
@@ -61,12 +72,16 @@ const VerifyCodePage = () => {
           square
         >
           <Box sx={MainVerifyBox}>
-            <Typography
-              color="#fff"
-              fontFamily="Lalezar"
-              component="h1"
-              variant="h5"
-            >
+            <Box display={"flex"} alignItems={"center"} mb={5}>
+              <Typography fontSize={"13px"}  color="#fff">
+                کد تایید برای شماره موبایل{token} ارسال گردید
+              </Typography>
+
+              <Link  to="/signup">
+                <EditIcon sx={{fontSize:"16px",color:"#111"}}/>
+              </Link>
+            </Box>
+            <Typography color="#fff" component="h1">
               کد تایید را وارد کنید
             </Typography>
             <Box
@@ -88,7 +103,7 @@ const VerifyCodePage = () => {
               />
             </Box>
 
-            <Box
+            {/* <Box
               sx={{
                 mt: 2,
                 px: 1,
@@ -104,7 +119,7 @@ const VerifyCodePage = () => {
                   ویرایش شماره همراه
                 </Button>
               </Link>
-            </Box>
+            </Box> */}
           </Box>
         </Grid2>
       </Grid2>
