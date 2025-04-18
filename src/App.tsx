5918f4b8f094f97fa3852d50a37b32696accafa7
emad <arabanemad@gmail.com>
2025-04-18 13:43:32 +0330

    remove ScrollToTop(Comp) and update App with ScrollToTop(Fun)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import ScrollToTop from "./components/modules/ScrollToTop";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import defaultOptions from "./configs/reactQuery";
 import { ToastProvider } from "./context/ToastProvider";
 import AppRoutes from "./routes/AppRoutes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+// تابع برای اسکرول به بالای صفحه
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
+
 function App() {
+  const { pathname } = useLocation();
+
+  // هر بار که مسیر تغییر کند، صفحه به بالای صفحه می‌رود
+  useEffect(() => {
+    scrollToTop(); // تابع اسکرول را فراخوانی می‌کنیم
+  }, [pathname]);
+
   const queryClient = new QueryClient({
     defaultOptions,
   });
 
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <ToastProvider>
-          <ScrollToTop />
-          <AppRoutes />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </ToastProvider>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <AppRoutes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ToastProvider>
+    </QueryClientProvider>
   );
 }
 
