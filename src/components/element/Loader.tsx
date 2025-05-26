76874d7d398a8d70e43ee0debe7593c07add2ee4
RezaFarzipour <rezafafa78@gmail.com>
2025-05-26 19:10:38 +0330

    loader added to homepage

---
+++ src/components/element/Loader.tsx
@@ -0,0 +1,34 @@
+import animationData from "../../assets/Animation - 1713359636723.json"
+import Lottie from "lottie-react";
+import {Box,Typography, Grid2} from '@mui/material'
+
+const Loader = () => {
+    return (  
+        <Grid2
+      sx={{
+        width: "100%",
+        height: "100vh",
+        zIndex: "10",
+        position: "fixed",
+        top: "0",
+        left: "0",
+        background: "#18181b",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid2 width="100%" flexDirection="column" className="svg-wrapper" display="flex" justifyContent="center" alignItems="center">
+        {/* copy svg image and past it here */}
+        <Box sx={{width:{xs:"30%",md:"12%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}} >
+          <Lottie animationData={animationData} />
+          <Typography sx={{fontSize:{xs:"15px",md:'20px',color:"#FFC436",fontFamily:"Yekan"}}}>طلای تهران </Typography>
+        </Box>
+     
+      
+      </Grid2>
+    </Grid2>
+    );
+}
+ 
+export default Loader;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import Homepage from "../components/template/homepage/HomeTemp";
+import Loader from "../components/element/Loader";
 
 const Home = () => {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    };
+    fakeDataFetch();
+  }, []);
+  return isLoading ? (
+    <Loader/>
+  ):(
     <div>
     <Homepage />
     </div>
