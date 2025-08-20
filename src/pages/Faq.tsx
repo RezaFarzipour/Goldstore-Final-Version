7efafaf2f9ed4570847cefb4046c196c7a94eba2
RezaFarzipour <rezafafa78@gmail.com>
2025-08-20 15:11:17 +0330

    faq

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -1,4 +1,4 @@
-import FaqsPage from "../components/template/faqpage/FaqsTemp"; 
+import FaqsPage from "../components/template/faqpage/FaqsTemp";
 
 const Faq: React.FC = () => {
   console.log("ff")
